25bc53e62b86d5931004748be290b5ec215a3b39
rogeriosenac <roge.pupo.toledo@gmail.com>
2025-03-14 09:24:44 -0300

    dowload pastas

---
+++ assetes/Js/json.js
@@ -0,0 +1,17 @@
+{
+    "comentarios": {
+      "1": {
+        "nome": "João Silva",
+        "foto": "https://example.com/joao.jpg",
+        "data": "2024-02-05 14:30:00",
+        "texto": "Ótimo atendimento, recomendo!"
+      },
+      "2": {
+        "nome": "Maria Souza",
+        "foto": "https://example.com/maria.jpg",
+        "data": "2024-02-06 10:15:00",
+        "texto": "Equipe muito profissional!"
+      }
+    }
+  }
+  
