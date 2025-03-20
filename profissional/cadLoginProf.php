14ace69983e64696dee8c2038a75a6593a003fd7
RogerioSenac <roge.pupo.toledo@gmail.com>
2025-03-20 11:53:05 -0300

    ajustes telas login

--- profissional/cadLoginProf.php
+++ profissional/cadLoginProf.php
@@ -84,15 +84,15 @@ function validarForcaSenha($senha)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Acesso</title>
-    <link rel="stylesheet" href="../assets/css/stylesCadLogin.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="icon" href="../assets/img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-    <div class="box">
-        <div class="formulario">
+    <div class="interface">
+        <div class="boxAcesso">
             <h2>Cadastro de Acesso</h2>
 
             <?php if (!empty($msg)) : ?>
