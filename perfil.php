3df36b725ad0caa0b138b6acd996bcdeac376437
RogerioSenac <roge.pupo.toledo@gmail.com>
2025-03-27 12:01:24 -0300

    ajustes responsividades

--- perfil.php
+++ perfil.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plataforma de Corte e Costura</title>
-    <link rel="stylesheet" href="../atelieconecta/assets/css/styles.css">
+    <!-- <link rel="stylesheet" href="../atelieconecta/assets/css/styles.css"> -->
+    <link rel="stylesheet" href="../atelieconecta/assets/css/stylesCadLogin.css">
     <link rel="icon" href="../atelieconecta/assets/img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
@@ -13,22 +14,21 @@
 </head>
 
 <body>
-    <main>
-        <div class="perfil d-flex flex-column" >
-            <div class="tipo-perfil">
-                <a href="./Cliente/loginCli.php">
-                    <img src="./assets/img/ic_woman_rosa.png" class="btn-perfil">
-                </a>
-                <p class="texto">Cliente</p>
-            </div>
-
-            <div class="tipo-perfil">
-                <a href="./profissional/loginProf.php">
-                    <img src="./assets/img/ic_profissional_rosa.png" class="btn-perfil">
-                </a>
-                <p class="texto">Profissional</p>
-            </div>
+    <div class="perfil d-flex flex-column">
+        <div class="tipo-perfil">
+            <a href="./Cliente/loginCli.php">
+                <img src="./assets/img/ic_woman_rosa.png" class="btn-perfil">
+            </a>
+            <p class="texto">Cliente</p>
         </div>
+
+        <div class="tipo-perfil">
+            <a href="./profissional/loginProf.php">
+                <img src="./assets/img/ic_profissional_rosa.png" class="btn-perfil">
+            </a>
+            <p class="texto">Profissional</p>
+        </div>
+    </div>
 </body>
 
 </html>
